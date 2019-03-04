cf3af25151921386e7dcf9066068a222fa107117
Angela Branaes <angela@proximistyle.com>
2019-03-04 16:51:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 /* Enter the code to remove the main node element under this comment */
 
+const main = document.getElementById("main");
+main.remove()
+
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerText = "Angela is the champion!";
+document.body.appendChild(newHeader);
